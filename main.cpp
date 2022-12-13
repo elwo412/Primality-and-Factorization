fca9f1192d4e0df6347c2865c150d7b7ba9eaa48
Elliott Worrall <eworrall@edgeaisolutions.com>
2022-12-13 15:28:48 -0500

    Successive squaring seemed to make it slower

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ extern "C" {
 using namespace std;
 using namespace NTL;
 
+NTL_CLIENT
+
 int td(ZZ n, long bound){
    //trial division
    PrimeSeq s;  // generates primes in sequence quickly
@@ -36,24 +38,47 @@ int step5(ZZ n, long r, long bound){
 
    ZZ_pX p_2Poly(1, 1); //X
    PowerMod(p_2Poly, p_2Poly, n, qmod); //X^n % X^r - 1
+   p_2Poly += 2;
 
+   ZZ_pX p_2Polycmp;
+
+   cout << bound << endl;
 
 
    while (a <= bound){
-      ZZX p_2_base;
-      p_2_base = ZZX(INIT_MONO, 1, 1); //(1)X^(1)
-      SetCoeff(p_2_base, 0, a); //X + a
-      ZZX p_2;
-      p_2 = ZZX(INIT_MONO, 1, r);
 
       //p_2Poly += a; // X^n + a
 
       ZZ_pX pPoly(1, 1); //X
       pPoly += a; //X + a
-      //NEED TO IMPLEMENT SUCCESSIVE SQUARING
-      PowerMod(pPoly, pPoly, n, qmod); //(X + a)^n % X^r - 1
 
-      if (pPoly != (p_2Poly + a)) return COMPOSITE;
+      ZZ_pX temp = ZZ_pX(pPoly);
+      //NEED TO IMPLEMENT SUCCESSIVE SQUARING
+      ///*
+      int k = 0;
+      long size_n_bits = NumBits(n);
+      //ZZ_pX *cached = (ZZ_pX *)malloc(sizeof(ZZ_pX)*size_n_bits);
+      //cached[0] = ZZ_pX(temp);
+      while (k < size_n_bits){
+         if (bit(n_bits, k) == 1){
+            //cout << "ADD TO TOTAL" << endl;
+            if (k != 0) pPoly = MulMod(pPoly, temp, qmod);
+            //cout << "[pPoly] LEADING DEG: " << deg(pPoly) << endl;
+         }  
+         temp = MulMod(temp, temp, qmod);
+         //cached[k] = ZZ_pX(temp);
+         //cout << "[temp] LEADING DEG: " << deg(temp) << endl;
+         k++;
+      }
+      //*/
+      //PowerMod(pPoly, pPoly, n, qmod); //(X + a)^n % X^r - 1
+      //cout << "[final] LEADING DEG: " << deg(pPoly) << endl;
+
+      SetCoeff(p_2Poly, 0, a);
+
+
+      
+      if (pPoly != p_2Poly) return COMPOSITE;
 
 
       //GEN p, p_2;
@@ -106,7 +131,7 @@ int main(int argc, char* argv[])
    long ln;
    while (fscanf(fp, "%s", n_str) != EOF) {
       ZZ n = conv<ZZ>(n_str);
-      if (n < 100) {ln = strtol(n_str,NULL,10); cout << "TD| " << n << ": " << td(n, ln) << endl; }
+      if (n < 10) {ln = strtol(n_str,NULL,10); cout << "TD| " << n << ": " << td(n, ln) << endl; }
       else
          cout << "AKS| " << n << ": " << aks(n, n_str) << endl;
    }
